1ce0276970e0b33d243074123bd848e94199f699
jason foster <j.foster@f5.com>
2025-04-23 16:23:10 +0100

    git pushing 5

--- smsv2.tf
+++ smsv2.tf
@@ -7,7 +7,7 @@ resource "volterra_securemesh_site_v2" "site" {
   logs_streaming_disabled = true
   enable_ha               = false
   labels = {
-    (volterra_known_label_key.key.key) = (volterra_known_label.label.value)
+    ("virtual-site-terraform") = (volterra_known_label.label.value)
     "ves.io/provider"                  = "ves-io-AZURE"
   }
 
@@ -188,7 +188,7 @@ resource "azurerm_network_interface_security_group_association" "inside_security
 #}
 
 resource "volterra_known_label" "label" {
-  key       = volterra_known_label_key.key.key
+  key       = "virtual-site-terraform"
   namespace = "shared"
   value     = local.f5xc_sms_name
 }
@@ -198,7 +198,7 @@ resource "volterra_virtual_site" "ce" {
   namespace = "shared"
 
   site_selector {
-    expressions = [format("%s = %s", volterra_known_label_key.key.key, volterra_known_label.label.value)]
+    expressions = [format("%s = %s", "virtual-site-terraform", volterra_known_label.label.value)]
   }
 
   site_type = "CUSTOMER_EDGE"
